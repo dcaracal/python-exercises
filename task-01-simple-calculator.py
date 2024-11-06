50cd1e186645f041d922221c91962830d6c8c6d9
Eduardo Melgarejo <eduardo.melgarejo@sollich.com>
2024-11-06 12:41:06 +0100

    dev: simple tasks no solution

--- task-01-simple-calculator.py
+++ task-01-simple-calculator.py
@@ -2,25 +2,4 @@
 #TODO: apply for the numbers. Implement summation, substraction, multiplication and division
 #TODO: Implement everything as a function
 
-def calculator():
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operator = input("Enter the operator (+, -, *, /): ")
-
-    if operator == '+':
-        result = num1 + num2
-    elif operator == '-':
-        result = num1 - num2
-    elif operator == '*':
-        result = num1 * num2
-    elif operator == '/':
-        if num2 == 0:
-            print("Error: Division by zero")
-        else:
-            result = num1 / num2
-    else:
-        print("Invalid operator")
-
-    print("Result:", result)
-
-calculator()
+#calculator()
